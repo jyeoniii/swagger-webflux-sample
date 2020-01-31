69e955d1ec59624a2261d9b95317de36b5ff6d21
jyeonkim <jyeonkim@linecorp.com>
2020-01-31 13:44:48 +0900

    Remove wrapper

--- src/main/java/com/example/swaggerdemo/controller/TestController.kt
+++ src/main/java/com/example/swaggerdemo/controller/TestController.kt
@@ -2,7 +2,6 @@ package com.example.swaggerdemo.controller
 
 import org.springframework.data.domain.Page
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
 
@@ -12,23 +11,12 @@ class Model
 @RestController
 class TestController {
     @GetMapping(value = ["/friend"], produces = ["application/json"])
-    fun getFriendList(): Mono<Wrapper<Page<Model>>> {
-        return Mono.just(
-            Wrapper(
-                Page.empty()
-            )
-        )
+    fun getFriendList(): Mono<Page<Model>> {
+        return Mono.just(Page.empty())
     }
 
     @GetMapping(value = ["/favorite"], produces = ["application/json"])
-    fun getFavorites(
-        @RequestParam(value = "page", defaultValue = "0") page: Int,
-        @RequestParam(value = "size", defaultValue = "10") size: Int
-    ): Mono<Wrapper<Page<Model>>> {
-        return Mono.just(
-            Wrapper(
-                Page.empty()
-            )
-        )
+    fun getFavorites(): Mono<Page<Model>> {
+        return Mono.just(Page.empty())
     }
 }
